fe763033728079675146906299c030edf1ab06f7
QuickApps <info@quickapps.es>
2015-01-25 06:23:32 +0100

    starting point

---
+++ README.md
@@ -0,0 +1,5 @@
+Disqus
+======
+
+Replaces your QuickAppsCMS comment system with your comments hosted and powered
+by Disqus.
---
+++ src/Event/DisqusHook.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Licensed under The GPL-3.0 License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @since    2.0.0
+ * @author   Christopher Castro <chris@quickapps.es>
+ * @link     http://www.quickappscms.org
+ * @license  http://opensource.org/licenses/gpl-3.0.html GPL-3.0 License
+ */
+namespace Disqus\Event;
+
+use Cake\Event\Event;
+use Cake\Event\EventListenerInterface;
+use QuickApps\Core\Plugin;
+
+/**
+ * Disqus Hook class.
+ *
+ */
+class DisqusHook implements EventListenerInterface
+{
+
+    /**
+     * Returns a list of hooks this Hook Listener is implementing. When the class is
+     * registered in an event manager, each individual method will be associated with
+     * the respective event.
+     *
+     * @return void
+     */
+    public function implementedEvents()
+    {
+        return [
+            'CommentHelper.beforeRender' => 'disqus',
+            'Plugin.Disqus.settingsDefaults' => 'settingsDefaults',
+        ];
+    }
+
+    /**
+     * Renders Disqus's comments.
+     *
+     * @param \Cake\Event\Event $event The event that was triggered
+     * @return string
+     */
+    public function disqus(Event $event)
+    {
+        $subject = $event->subject(); // comment helper        
+        if ($subject->config('visibility') > 0) {
+            $event->stopPropagation();
+            $settings = Plugin::settings('Disqus');
+
+            if (!empty($settings['disqus_shortname'])) {
+                return $subject->_View->element('Disqus.js', ['settings' => $settings]);
+            } else {
+                return '<!-- Disqus plugin not configured -->';
+            }
+        }
+    }
+
+    public function settingsDefaults()
+    {
+        return [
+            'disqus_shortname' => null,
+        ];
+    }
+}
